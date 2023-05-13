22de1a89b73f07bbae21f53eac07845e77722c46
Vladimir Kvachevskii <pro@itremedy.pro>
2023-05-13 23:12:25 +0400

    added layouts

--- app/src/main/java/com/itremedy/improvidermtfree/GuestAttention.java
+++ app/src/main/java/com/itremedy/improvidermtfree/GuestAttention.java
@@ -1,11 +1,8 @@
 package com.itremedy.improvidermtfree;
 
-import static com.itremedy.improvidermtfree.ConnectionManager.result;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
